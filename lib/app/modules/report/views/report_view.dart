7354f1ce92a66a3df9efd87387f3dc1989a07d91
bagussamito <baggusamit@gmail.com>
2022-07-15 14:54:25 +0700

    nambah sitik

--- lib/app/modules/report/views/report_view.dart
+++ lib/app/modules/report/views/report_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
+import 'package:project_magang/app/modules/home/views/home_view.dart';
+import 'package:project_magang/app/widgets/custom_icon_home_icons.dart';
 import 'package:get/get.dart';
-
+import 'package:project_magang/app/theme/theme.dart';
 import '../controllers/report_controller.dart';
 
 class ReportView extends GetView<ReportController> {
@@ -10,14 +13,66 @@ class ReportView extends GetView<ReportController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ReportView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ReportView is working',
-          style: TextStyle(fontSize: 20),
+        toolbarHeight: 82,
+        title: Image.asset(
+          'assets/icons/icon.png',
+          fit: BoxFit.cover,
+          width: 200,
+          height: 35,
         ),
+        centerTitle: true,
+        backgroundColor: Blue1,
+      ),
+      body: FooterView(
+        children: <Widget>[
+          SizedBox(
+            height: 320,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // Attendance
+              Container(
+                height: 112,
+                width: 174,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Blue1,
+                    border: Border.all(width: 5.0, color: Yellow1)),
+                child: ElevatedButton(
+                  onPressed: () => Get.to(HomeView()),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(CustomIconHome.report, color: Yellow1, size: 38),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        'New Report',
+                        style: TextStyle(fontSize: 16),
+                      )
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
+              ),
+              // Location
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+        ],
+        footer: new Footer(
+          backgroundColor: backgroundBlue,
+          child: Container(),
+        ),
+        flex: 2,
       ),
     );
   }
--- lib/app/modules/setting/views/setting_view.dart
+++ lib/app/modules/setting/views/setting_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
 
 import 'package:get/get.dart';
 
+import '../../../theme/theme.dart';
+import '../../../widgets/custom_icon_home_icons.dart';
 import '../controllers/setting_controller.dart';
 
 class SettingView extends GetView<SettingController> {
@@ -10,14 +14,77 @@ class SettingView extends GetView<SettingController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('SettingView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'SettingView is working',
-          style: TextStyle(fontSize: 20),
+        toolbarHeight: 82,
+        title: Image.asset(
+          'assets/icons/icon.png',
+          fit: BoxFit.cover,
+          width: 200,
+          height: 35,
         ),
+        centerTitle: true,
+        backgroundColor: Blue1,
+      ),
+      body: FooterView(
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ClipOval(
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.grey.shade200,
+                  child: Center(child: Text("X")),
+                  // child: Image.network(src),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "ARIEL SIMANJUTAK",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Icon(CustomIconHome.attendance, color: Yellow1, size: 38)
+                    ],
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    "BackEnd Developer",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    "arielsimanjutak@gmail.com",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Column()
+            ],
+          ),
+        ],
+        footer: new Footer(
+          backgroundColor: backgroundBlue,
+          child: Container(),
+        ),
+        flex: 3,
       ),
     );
   }
