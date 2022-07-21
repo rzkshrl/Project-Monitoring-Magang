9c402fef3adf154ef03d1848353482205256d6b5
bagussamito <baggusamit@gmail.com>
2022-07-21 09:37:08 +0700

    setting

--- lib/app/modules/setting/views/setting_view.dart
+++ lib/app/modules/setting/views/setting_view.dart
@@ -3,6 +3,7 @@ import 'package:footer/footer.dart';
 import 'package:footer/footer_view.dart';
 
 import 'package:get/get.dart';
+import 'package:project_magang/app/widgets/custom_icon_login_icons.dart';
 
 import '../../../theme/theme.dart';
 import '../../../widgets/custom_icon_home_icons.dart';
@@ -73,11 +74,109 @@ class SettingView extends GetView<SettingController> {
                   ),
                 ],
               ),
-              SizedBox(
-                width: 30,
-              ),
-              Column()
             ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Grey1, borderRadius: BorderRadius.circular(10)),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Pengaturan Akun",
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(width: 350, height: 2, color: dark),
+                  Container(
+                    width: 365,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Grey1, borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      style: TextStyle(color: dark),
+                      decoration: InputDecoration(
+                          prefixIcon: Align(
+                              widthFactor: 1.0,
+                              heightFactor: 1.0,
+                              child: Icon(
+                                CustomIconLogin.mail,
+                                color: dark,
+                              )),
+                          hintText: 'Email',
+                          hintStyle: heading6.copyWith(color: dark),
+                          border:
+                              OutlineInputBorder(borderSide: BorderSide.none)),
+                    ),
+                  ),
+                  Container(width: 350, height: 2, color: dark),
+                  Container(
+                    width: 370,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Grey1, borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      style: TextStyle(color: dark),
+                      // obscureText: controller.isPasswordHidden.value,
+                      // controller: passC,
+                      decoration: InputDecoration(
+                          prefixIcon: Align(
+                              widthFactor: 1.0,
+                              heightFactor: 1.0,
+                              child: Icon(
+                                CustomIconLogin.lock,
+                                color: dark,
+                              )),
+                          hintText: 'Password',
+                          hintStyle: heading6.copyWith(color: dark),
+                          // suffixIcon: IconButton(
+                          //   color: dark,
+                          //   splashRadius: 1,
+                          //   icon: Icon(controller.isPasswordHidden.value
+                          //       ? Icons.visibility_outlined
+                          //       : Icons.visibility_off_outlined),
+                          //   onPressed: () {
+                          //     controller.isPasswordHidden.value =
+                          //         !controller.isPasswordHidden.value;
+                          //   },
+                          // ),
+                          border:
+                              OutlineInputBorder(borderSide: BorderSide.none)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 75,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Blue1,
+                        border: Border.all(width: 5.0, color: Yellow1)),
+                    child: TextButton(
+                      onPressed: () {},
+                      /*authC.login(emailC.text, passC.text)*/
+                      child: Text(
+                        'Logout',
+                        style: headingBtn.copyWith(color: Yellow1),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ]),
           ),
         ],
         footer: new Footer(
